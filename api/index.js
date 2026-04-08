f197ba55dd63472a251fa2d99949e22e94b4fac0
agustin <agustin_panizza@hotmail.com>
2026-04-08 16:46:46 -0300

    backend fix

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // imports
-require("dotenv").config();
+require("dotenv").config({ path: require("path").resolve(__dirname, ".env") });
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -16,7 +16,6 @@ const port = process.env.PORT || 4000;
 const db = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/taller";
 
 app.use(cors());
-app.use(errorHandler);
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,26 +23,26 @@ app.use(bodyParser.json());
 
 // routes
 app.use("/taller/", router);
+app.use(errorHandler);
 
 // connect to DB & listen
-
-const connectDB = () => {
+const connectDB = async () => {
   try {
-    mongoose.connect(db, {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
     app.listen(port, () => {
-      console.log(`Server on port ${port} and connected to DB 🔌`);
+      console.log(
+        `Server on port ${port} and connected to DB ${mongoose.connection.name}`
+      );
     });
   } catch (err) {
-    console.log("Error al conectar a la db 🚫");
+    console.log("Error al conectar a la db");
     console.error(err.message);
     process.exit(1);
   }
 };
 
 connectDB();
-
-//
